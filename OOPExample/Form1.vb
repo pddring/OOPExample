b9bdfb626eb9eda2f1a330b049afdfe86d0d8ac0
Pete Dring <pddring@googlemail.com>
2019-01-14 21:47:50 +0000

    Dog works

--- OOPExample/Form1.vb
+++ OOPExample/Form1.vb
@@ -1,11 +1,15 @@
 ﻿Public Class Form1
 
-    Sub addDog() Handles btnAddDog.Click
+    Private Sub btnAddDog_Click(sender As Object, e As EventArgs) Handles btnAddDog.Click
         Dim dog As New Dog(InputBox("Name of dog: "))
         lstAnimals.Items.Add(dog)
     End Sub
 
-    Private Sub btnAddDog_Click(sender As Object, e As EventArgs)
-
+    Private Sub btnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        For i = 0 To lstAnimals.Items.Count - 1
+            lstAnimals.SelectedIndex = i
+            Dim pet As Animal = lstAnimals.Items(i)
+            pet.makeSound()
+        Next
     End Sub
 End Class
